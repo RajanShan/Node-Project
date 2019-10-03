df236a343c344b1f656f90c6df3c334a82b5e862
Madhusudan Malik <Madhusudanmmalik1795@gmail.com>
2019-10-03 17:24:33 +0530

    Chage PORT from port

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ var server = http.createServer(function (req, res) {
     }
     res.end();
 });
-var port=process.env.port||3000;
+var port=process.env.PORT||3000;
 server.listen(port, function () {
     console.log("server is running at port 3000");
 })
